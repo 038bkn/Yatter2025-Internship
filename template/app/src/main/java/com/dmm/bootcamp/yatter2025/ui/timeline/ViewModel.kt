5e3019fbc8f1ae9980c6fdd977357f0c71233b44
038bkn <038bkn@gmail.com>
2025-07-30 12:01:53 +0900

    UiStateの公開まで

---
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/timeline/ViewModel.kt
@@ -0,0 +1,12 @@
+package com.dmm.bootcamp.yatter2025.ui.timeline
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class PublicTimelineViewModel : ViewModel() {
+    private val _uiState: MutableStateFlow<PublicTimelineUiState> =
+        MutableStateFlow(PublicTimelineUiState.empty())
+    val uiState: StateFlow<PublicTimelineUiState> = _uiState.asStateFlow()
+}
