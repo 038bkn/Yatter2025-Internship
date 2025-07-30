8045d1f25b7d493dbdc177284e0a28e69149c768
038bkn <038bkn@gmail.com>
2025-07-30 16:12:41 +0900

    リスト表示

--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/timeline/PublicTimelineTemplate.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/timeline/PublicTimelineTemplate.kt
@@ -1,10 +1,27 @@
 package com.dmm.bootcamp.yatter2025.ui.timeline
 
+import android.graphics.Paint.Align
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Surface
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.PullRefreshState
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dmm.bootcamp.yatter2025.ui.theme.Yatter2025Theme
 import com.dmm.bootcamp.yatter2025.ui.timeline.bindingmodel.YweetBindingModel
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun PublicTimelineTemplate(
     yweetList: List<YweetBindingModel>,
@@ -12,9 +29,33 @@ fun PublicTimelineTemplate(
     isRefreshing: Boolean,
     onRefresh: () -> Unit,
 ) {
-
+    val pullRefreshState = rememberPullRefreshState(isRefreshing, onRefresh)
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .pullRefresh(pullRefreshState),
+        contentAlignment = Alignment.Center,
+    ) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(8.dp),
+    ) {
+        items(yweetList) { item ->
+            YweetRow(yweetBindingModel = item)
+        }
+    }
+    PullRefreshIndicator(
+        refreshing = isRefreshing,
+        state = pullRefreshState,
+        modifier = Modifier.align(Alignment.TopCenter)
+    )
+    if (isLoading) {
+        CircularProgressIndicator()
+    }
+}
 }
 
+@Preview
 @Composable
 private fun PublicTimelineTemplatePreview() {
     Yatter2025Theme {
@@ -38,7 +79,7 @@ private fun PublicTimelineTemplatePreview() {
                         attachmentImageList = listOf()
                     ),
                 ),
-                isLoading = true,
+                isLoading = false,
                 isRefreshing = false,
                 onRefresh = {}
             )
