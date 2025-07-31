9d7922bb6d86b00dabb324b3653450de4336d8ce
038bkn <038bkn@gmail.com>
2025-07-31 17:06:40 +0900

    UiStateの実装

--- template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/post/PostUiState.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter2025/ui/post/PostUiState.kt
@@ -1,3 +1,21 @@
 package com.dmm.bootcamp.yatter2025.ui.post
 
-data class PostUiState()
+import com.dmm.bootcamp.yatter2025.ui.post.bindingmodel.PostBindingModel
+
+data class PostUiState(
+    val bindingModel: PostBindingModel,
+    val isLoading: Boolean,
+) {
+    companion object {
+        fun empty(): PostUiState = PostUiState(
+            bindingModel = PostBindingModel(
+                avatarUrl = null,
+                yweetText = ""
+            ),
+            isLoading = false,
+        )
+    }
+
+    val canPost: Boolean
+        get() = bindingModel.yweetText.isNotBlank()
+}
